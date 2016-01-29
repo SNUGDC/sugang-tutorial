b1200b1a20bb4330dc3f21151d106915298ce172
Phillip Chang <lasagnaphil@snu.ac.kr>
2016-01-29 23:44:45 +0900

    Added desktop and clickable icons

---
+++ Assets/Scripts/Icon.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Icon : MonoBehaviour
+{
+    private Image iconImage;
+    public Text iconText;
+    public GameObject iconSelectedEffect;
+    
+    public DesktopManager desktopManager;
+    
+    float doubleClickStart = 0;
+    public bool isSelected = false;
+    
+    void Start()
+    {
+        iconImage = GetComponent<Image>();
+    }
+    void Update()
+    {
+        iconSelectedEffect.SetActive(isSelected);
+    }
+    
+    public void OnMouseClickUp()
+    {
+        // Double click detection code
+        if ((Time.time - doubleClickStart) < 0.3f)
+        {
+            this.OnDoubleClick();
+            doubleClickStart = -1;
+        }
+        else
+        {
+            doubleClickStart = Time.time;
+        }
+        
+        desktopManager.deselectIcons();
+        isSelected = true;
+    }
+    
+    void OnDoubleClick()
+    {
+        Debug.Log("Double clicked!");
+    }
+}
