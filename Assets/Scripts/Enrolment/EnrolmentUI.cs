a823c0ecf42d21a83b860aafd8403738edaee1d1
ParkJuhyeong <majecty@gmail.com>
2016-01-30 17:33:57 +0900

    Refactor enrolmentUI.

--- Assets/Scripts/Enrolment/EnrolmentUI.cs
+++ Assets/Scripts/Enrolment/EnrolmentUI.cs
@@ -8,18 +8,28 @@ public class EnrolmentUI : MonoBehaviour {
     // example data.
     private List<Subject> subjects = new List<Subject>();
     
-    public void Start() {
-        addSubject(code: "001", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "002", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "003", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "004", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "005", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "006", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "007", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "008", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "009", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        addSubject(code: "010", name: "Let's make game.", department: "itct", maxEnrolment: 20);
-        
+    private void Start() {
+        List<Subject> exampleSubjects = new List<Subject> {
+            newSubject(code: "001", name: "Let's make game.", department: "itct"),
+            newSubject(code: "002", name: "Let's make game.", department: "itct"),
+            newSubject(code: "003", name: "Let's make game.", department: "itct"),
+            newSubject(code: "004", name: "Let's make game.", department: "itct"),
+            newSubject(code: "005", name: "Let's make game.", department: "itct"),
+            newSubject(code: "006", name: "Let's make game.", department: "itct"),
+            newSubject(code: "007", name: "Let's make game.", department: "itct"),
+            newSubject(code: "008", name: "Let's make game.", department: "itct"),
+            newSubject(code: "009", name: "Let's make game.", department: "itct"),
+            newSubject(code: "010", name: "Let's make game.", department: "itct")
+        };
+
+        Setup(exampleSubjects);
+    }
+    public void Setup(List<Subject> subjectsInput) {
+        this.subjects = subjectsInput;
+        foreach(var subject in subjectsInput) {
+            EnrolmentSingleton.Instance.DB.Set(subject, maxEnrolment: 20);    
+        }
+
         foreach(var subject in subjects) {
             var row = Instantiate<SubjectRow>(subjectResultRowExample);
             row.transform.SetParent(subjectRowParent.transform);
@@ -30,13 +40,12 @@ public class EnrolmentUI : MonoBehaviour {
         }
     }
 
-    public void addSubject(string code, string name, string department, int maxEnrolment) {
+    private Subject newSubject(string code, string name, string department) {
         var subject = new Subject();
         subject.code = code;
         subject.name = name;
         subject.department = department;
         
-        subjects.Add(subject);
-        EnrolmentSingleton.Instance.DB.Set(subject, maxEnrolment);
+        return subject;
     }
 }
