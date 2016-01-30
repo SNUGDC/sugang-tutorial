b2c416863db7d728bb931f05961be820c5357c3e
ParkJuhyeong <majecty@gmail.com>
2016-01-30 17:19:33 +0900

    Implement enrolment fake data.

---
+++ Assets/Scripts/Enrolment/EnrolmentSingleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnrolmentSingleton {
+    private static EnrolmentSingleton instance;
+    public static EnrolmentSingleton Instance {
+        get {
+            if (instance == null)
+            {
+                instance = new EnrolmentSingleton();
+            }
+            return instance;
+        }
+        set {
+            instance = value;
+        }
+    }
+    
+    public EnrolmentDB DB = new EnrolmentDB();
+}
