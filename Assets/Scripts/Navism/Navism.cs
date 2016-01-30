d08bff9fdd469dad8515586efdb6c11737f1da81
ParkJuhyeong <majecty@gmail.com>
2016-01-31 06:40:04 +0900

    Implement navism sound.

---
+++ Assets/Scripts/Navism/Navism.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Navism : MonoBehaviour {
+    public NavismDateTimeText navismDateTimeText;
+    public void Start()
+    {
+        var now = System.DateTime.Now;
+        var before10Minuate = new DateTime(
+            year: now.Year,
+            month: now.Month,
+            day: now.Day,
+            hour: 7,
+            minute: 59,
+            second: 50);
+            
+        navismDateTimeText.Setup(before10Minuate, before10Minuate.AddSeconds(1));
+    }
+}
---
+++ Assets/Scripts/Navism/NavismDateTimeText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class NavismDateTimeText : MonoBehaviour {
+    public Text dateTimeText;
+    public DateTime dateTime = System.DateTime.Now;
+    public AudioSource audioSource;
+    public DateTime soundStartTime;
+
+	void Update () {
+	   var dateTimeString = dateTime.ToString("yyyy년 MM월 dd일 HH시 mm분 ss초");
+       dateTimeText.text = dateTimeString;
+
+       var previousTime = dateTime;
+       dateTime = dateTime.AddSeconds(Time.deltaTime);
+
+       if (previousTime <= soundStartTime && soundStartTime < dateTime)
+       {
+           audioSource.gameObject.SetActive(true);
+       }
+	}
+
+    public void Setup(DateTime currentTime, DateTime soundStartTime)
+    {
+        dateTime = currentTime;
+        this.soundStartTime = soundStartTime;
+    }
+}
