3f92df2e7a0a3428615867e8059a1039f1de6c54
Lee, Kyuho <modulo641@gmail.com>
2016-01-30 20:40:37 +0900

    class for student's information

---
+++ Assets/Scripts/MyUnivData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+public class MyUnivData 
+{
+    public string name;
+    public string studentID;
+    public int semester;
+    public string major;
+    public string secondaryMajor;
+    public List<Subject> finishedSubjects;
+}
