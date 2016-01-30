003ee76758570c498d4c0bf1f3e98551ebd60cf8
Lee, Kyuho <modulo641@gmail.com>
2016-01-30 17:58:51 +0900

    Fixed browser

--- Assets/Scripts/Browser/MirrorText.cs
+++ Assets/Scripts/Browser/MirrorText.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class MirrorText : MonoBehaviour 
+public class MirrorText : MonoBehaviour
 {
     public Text source;
     public Text output;
     
-    void Start()
+    public void OnEnable()
     {
         output.text = "· 웹 주소  " + source.text + "  이(가) 정확한지 확인하세요";
     }
